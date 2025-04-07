6a253371bf9263d4595d8d155dc81f7fc7401e41
Jon Johansson <jonjohansson88@gmail.com>
2025-04-07 13:31:36 +0200

    Add project files.

---
+++ MarketAPI/Controllers/AddController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
